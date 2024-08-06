044d311e901c2ab7b0c30ad500182a48063698f7
ShahdHatem55 <p60508302@gmail.com>
2024-08-06 15:33:23 +0300

    commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_main)
+
+
+
+
+    }
+}
